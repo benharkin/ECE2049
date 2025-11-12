f8ce8567f10f15ab5b1524b8ea1a3b58c37d0449
benharkin <bharkin@wpi.edu>
2025-11-12 14:21:09 -0500

    Clarifying comments

--- note.h
+++ note.h
@@ -19,7 +19,7 @@
 #define A1 7
 
 // These are the denominator - 1. So a whole note has a denominator of 1,
-// so it is defined here as 0.
+// so it is defined here as 0. Shifted right 3 to make room for the pitch bits
 #define WN (0 << 3)
 #define HN (1 << 3)
 #define QN (3 << 3)
@@ -30,9 +30,17 @@
 // Usage:
 // A0 | HN is an A0 half note.
 
+// Note type, since we are using a special bit layout
 typedef char Note;
+
+// Returns the period of the note in quartz ticks
 int getPeriod(Note n);
+
+// Returns the length of the note in quartz ticks
 int getDuration(Note n);
+
+// Returns a char with the least significant 4 bits representing the status of the LEDS for the note.
+// The LSB is the leftmost and the MSB is the rightmost. Can change this if necessary.
 char getLED(Note n);
 
 #endif /* NOTE_H_ */
