cac2c308e1c06389b97d17f90dd1b9f0491da90c
benharkin <bharkin@wpi.edu>
2025-11-12 14:22:45 -0500

    Autoformatted, added '#' reset key, countdown state (untested).

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "timer.h"
 
 #define SONG_LENGTH 28
+#define COUNTDOWN_LENGTH 3
 
 void print_str(char *str, int32_t x, int32_t y);
 
@@ -22,14 +23,14 @@ void main(void)
     configKeypad();
     initButtons();
 
-
     char song[SONG_LENGTH];
 
     state_t state = IDLE;
     state_t prev_state = NONE;
 
-    const int countdown_start = 3;
-    int countdown = countdown_start;
+    long countdown_start;
+    int countdown_elapsed = 0;
+    int countdown_elapsed_prev = 0;
 
     int note_end;
 
@@ -43,13 +44,22 @@ void main(void)
 
     char stored_input;
 
-    while (1){
+    while (1)
+    {
 
         key = getKey();
         button = getButtons();
 
-        switch (state){
+        // # key resets the game to IDLE
+        if (key != prevKey && key == '#')
+        {
+            state = IDLE;
+        }
+
+        switch (state)
+        {
         case IDLE:
+        {
             //Display welcome screen, wait for *
             if (state != prev_state)
             {
@@ -62,49 +72,85 @@ void main(void)
                 state = COUNTDOWN;
             }
             break;
+        }
         case COUNTDOWN:
+        {
             //Countdown 3 2 1 and then move to playing state
+            if (state != prev_state)
+            {
+                countdown_start = millis();
+                countdown_elapsed = 0;
+                countdown_elapsed_prev = 0;
+            }
+
+            countdown_elapsed = (millis() - countdown_start) / 1000; // Seconds
+            int countdown = COUNTDOWN_LENGTH - countdown_elapsed;
+
+            if(countdown == 0){
+                state = PLAYING;
+                break;
+            }
+
+            if(countdown_elapsed != countdown_elapsed_prev){
+                char countdown_str[] = { countdown + 48, '\0' };
+                print_str(countdown_str, 48, 48);
+            }
+
+            countdown_elapsed_prev = countdown_elapsed;
             break;
+        }
         case PLAYING:
+        {
             //
             //Loop through the song array
             //On each loop
 
-            if(getTime()<note_end){
+            if (getTime() < note_end)
+            {
                 //note is still playing
                 //check if button input matches the note
                 stored_input |= button;
-            } else {
+            }
+            else
+            {
                 BuzzerOff();
                 setLeds(0);
 
-                if(stored_input != song[current_note] & (BIT0|BIT1|BIT2|BIT3)){
+                if (stored_input != song[current_note]
+                        & (BIT0 | BIT1 | BIT2 | BIT3))
+                {
                     //Wrong note, game over
                     state = GAME_OVER;
                     break;
                 }
 
-                if (current_note == SONG_LENGTH - 1){
+                if (current_note == SONG_LENGTH - 1)
+                {
                     state = WIN;
                     break;
                 }
 
                 current_note++;
                 BuzzerOn(song[current_note]);
-                setLeds(song[current_note] & (BIT0|BIT1|BIT2|BIT3));
+                setLeds(song[current_note] & (BIT0 | BIT1 | BIT2 | BIT3));
                 note_end = getTime() + getDuration(song[current_note]);
 
                 //play note at current_note index (buzzer and corresponding LED)
                 //set note_end to timer + note duration
             }
             break;
+        }
         case GAME_OVER:
+        {
             //Display game over and after 1 second go to idle
             //Play a losing jingle
             break;
+        }
         case WIN:
+        {
             //Display win and after 1 second go to idle
             break;
+        }
         }
 
         prevKey = key;
