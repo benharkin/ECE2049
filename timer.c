83f450af4d9549c1ab5785601ead47cf1f6f9f56
benharkin <bharkin@wpi.edu>
2025-12-05 14:10:35 -0500

    Added timedate struct, math, setters, getters to timer.

--- timer.c
+++ timer.c
@@ -6,7 +6,8 @@
 
 static volatile unsigned long timer;
 
-void setupTimer(void){
+void setupTimer(void)
+{
     //Timer A2 setup for interrupts every 1/128 second
     TA2CTL = TASSEL_1 | ID_0 | MC_1 | TACLR;
     TA2CCR0 = (32768 / TICKSPS) - 1;
@@ -14,7 +15,8 @@ void setupTimer(void){
 }
 
 // Returns the time in multiples of 1/TICKSPS seconds
-unsigned long getTime(void){
+unsigned long getTime(void)
+{
 //    __disable_interrupt();
 //    unsigned long temp = timer;
 //    __enable_interrupt();
@@ -22,11 +24,13 @@ unsigned long getTime(void){
 }
 
 // Returns the time in seconds
-float getSecondsF(void){
-    return getTime() / (float)TICKSPS;
+float getSecondsF(void)
+{
+    return getTime() / (float) TICKSPS;
 }
 
-unsigned int getSeconds(void){
+unsigned long getSeconds(void)
+{
     return getTime() / TICKSPS;
 }
 
@@ -37,3 +41,42 @@ __interrupt void Timer_A2_ISR(void)
     //TA2CCTL0 &= ~CCIFG;
 }
 
+timedate_t getTimeDate(void)
+{
+    return secondsToTimeDate(getSeconds());
+}
+
+void setTimer(unsigned long ticks)
+{
+    timer = ticks;
+}
+void setSeconds(unsigned long seconds)
+{
+    timer = seconds * TICKSPS;
+}
+void setTime(timedate_t time)
+{
+    setTimer(timeDateToSeconds(time));
+}
+
+timedate_t secondsToTimeDate(unsigned long seconds)
+{
+    unsigned char minutes = seconds / 60;
+    unsigned char hours = minutes / 60;
+    unsigned char days = hours / 24;
+    unsigned char months = days / 30;
+    timedate_t out = { months % 12, days % 30, hours % 24, minutes % 60, seconds
+                               % 60 };
+    return out;
+
+}
+
+unsigned long timeDateToSeconds(timedate_t td)
+{
+    unsigned long out = td.second;
+    out += td.minute * 60;
+    out += td.hour * 3600;
+    out += td.day * 86400;
+    out += td.month * 2592000;
+    return out;
+}
--- timer.h
+++ timer.h
@@ -1,12 +1,27 @@
 #ifndef TIMER_H_
 #define TIMER_H_
 
-#define TICKSPS 128 // The number of timer ticks per second.
+#define TICKSPS 1 // The number of timer ticks per second.
+
+typedef struct {
+    unsigned char month; // Jan = 1, Dec = 12
+    unsigned char day; // First day of month = 1
+    unsigned char hour; // 12AM = 0, 24 hour clock
+    unsigned char minute; // xx:00 = 0
+    unsigned char second; // xx:xx:00 = 0
+} timedate_t;
 
 void setupTimer(void);
 unsigned long getTime(void);
 float getSecondsF(void);
-unsigned int getSeconds(void);
+unsigned long getSeconds(void);
+timedate_t getTimeDate(void);
+void setTimer(unsigned long ticks);
+void setSeconds(unsigned long seconds);
+void setTime(timedate_t time);
+
+timedate_t secondsToTimeDate(unsigned long inSeconds);
+unsigned long timeDateToSeconds(timedate_t td);
 
 
 #endif /* TIMER_H_ */
