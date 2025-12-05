9ccd4ba71dbc3263e1da15382a7093ce665baa10
Leo Shraybman <90734146+Leololo8@users.noreply.github.com>
2025-12-05 18:39:46 -0500

    Working sensor.c

--- sensor.c
+++ sensor.c
@@ -8,40 +8,72 @@
 #include <msp430.h>
 #include "sensor.h"
 
+unsigned int in_current,in_temp;
+volatile float milliamps, tempC;
+float tempC_array[MOVING_AVG_LENGTH];
 
-unsigned int in_temp;
-float tempC[MOVING_AVG_LENGTH];
-volatile float temperatureDegC, temperatureDegF, degC_per_bit;
 
 void setupADC(void){
-    degC_per_bit = ((float)(85.0 – 30.0))/ ((float)(CALADC12_15V_85C-CALADC12_15V_30C));
-    // Reset REFMSTR to hand over control of internal reference
-    // voltages to ADC12_A control registers
     REFCTL0 &= ~REFMSTR;
-    // Internal ref is on and set to 1.5V
-    ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON;
-    ADC12CTL1 = ADC12SHP; // Enable sample timer
-    ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_10;
-    __delay_cycles(100); // delay to allow Ref to settle
-    ADC12CTL0 |= ADC12ENC; // Enable conversion
+
+    ADC12CTL0=ADC12SHT0_9|ADC12REFON|ADC12REF2_5V|ADC12ON|ADC12MSC;
+
+    ADC12CTL1 = ADC12SHP+ADC12CONSEQ_1;
+
+    ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_0;
+    // ADC12MCTL1: EOS = 1, SREF =001 = voltage refs = GND to Vref+
+    // INCHx = 1010
+    ADC12MCTL1 = ADC12SREF_1 + ADC12INCH_10 + ADC12EOS;
+    // Set Port 6 Pins 0 to FUNCTION mode (=1) for ADC12
+    // (Current sensor input is on analog input A0)
+    P6SEL = P6SEL | BIT0;
+}
+float get_Temp_C(void){
+    ADC12CTL0 |= ADC12SC + ADC12ENC;
+    while (ADC12CTL1 & ADC12BUSY) // poll busy bit
+        __no_operation();
+    in_temp = ADC12MEM1 & 0x0FFF; // keep only low 12 bits
+    tempC = (float)(((long)in_temp-CALADC12_25V_30C)*(85 - 30))/
+            (CALADC12_25V_85C - CALADC12_25V_30C) + 30.0;
+    return tempC;
 }
 
-float get_temp_C(void){
-    ADC12CTL0 &= ~ADC12SC; // clear the start bit
-    ADC12CTL0 |= ADC12SC; // Sampling and conversion start
-    // Single conversion (single channel)
-    // Poll busy bit waiting for conversion to complete
-    while (ADC12CTL1 & ADC12BUSY){
-        __no_operation();
+void updateTempAverage(unsigned long time){
+    //run every second
+    float new_temp = get_Temp_C();
+    int average_time_idx = time % MOVING_AVG_LENGTH;
+
+    tempC_array[average_time_idx] = new_temp;
+}
+
+float get_Temp_AVG_C(void){
+    float sum = 0.0;
+    int non_zero_temps = 0;
+    for (int i = 0; i < MOVING_AVG_LENGTH; i++){
+        if (tempC_array[i] != 0){
+            sum += tempC_array[i];
+            non_zero_temps++;
+        }
     }
 
-    in_temp = ADC12MEM0; // Read results from conversion
-    temperatureDegC=(float)(((long)in_temp-CALADC12_15V_30C) *degC_per_bit + 30.0;
-    // Temperature in Fahrenheit = (9/5)*Tc + 32
-    //temperatureDegF = temperatureDegC * 9.0/5.0 + 32.0;
-    __no_operation(); // SET BREAKPOINT HERE
-    return temperatureDegC;
+    return sum / non_zero_temps;
 }
+float get_Temp_AVG_F(void){
+    float temp_C = get_Temp_AVG_C();
+    return (temp_C * 9/5) + 32;
+}
+int get_Pot_Scaled(int min, int max){
+    ADC12CTL0 |= ADC12SC + ADC12ENC;
+    while (ADC12CTL1 & ADC12BUSY) // poll busy bit
+        __no_operation();
+
+    in_current = ADC12MEM0 & 0x0FFF;
+    //Scale in_current to the range min-max
+    int scaled = min + ((in_current * (max - min)) / 4095);
+
+    return scaled;
+}
+
 
 
 
